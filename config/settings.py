d87f08f9782bcc486c24564b4e698328965cf807
sergei_vokhminov <rreaper@list.ru>
2025-04-24 16:11:43 +0300

    корректировка настроек проекта

--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "users",
 ]
 
 # Список промежуточного ПО, которое обрабатывает входящие запросы и выходящие ответы.
