5dca5c029ab1a38665544d299d2d723f6d867481
sergei_vokhminov <rreaper@list.ru>
2025-04-26 10:03:07 +0300

    обновлен модуль views приложения users

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from users.models import User
+
+
+class HomeView(TemplateView):
+    """Контроллер представления главной страницы."""
+
+    template_name = "users/home.html"
