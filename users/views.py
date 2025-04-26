1f715858664d9f1a37a2a344c62466fcb0f73923
sergei_vokhminov <rreaper@list.ru>
2025-04-26 15:57:55 +0300

    добавлено представление для регистрации

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView
 
 from users.models import User
 
@@ -7,3 +8,11 @@ class HomeView(TemplateView):
     """Контроллер представления главной страницы."""
 
     template_name = "users/home.html"
+
+
+class UserRegisterView(CreateView):
+    """Контроллер регистрации профиля."""
+
+    model = User
+    template_name = "users/register.html"
+    success_url = reverse_lazy("users:login")
