83518400f77836eb58902b95e6c2919fe35ce947
sergei_vokhminov <rreaper@list.ru>
2025-04-26 15:57:23 +0300

    обновлены маршруты приложения users

--- users/urls.py
+++ users/urls.py
@@ -3,11 +3,14 @@ from django.urls import path
 
 from users.apps import UsersConfig
 from users.views import (
-    HomeView,
+    HomeView, UserRegisterView,
 )
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
+    path("login/", LoginView.as_view(template_name="users/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(next_page="users:home"), name="logout"),
+    path("register/", UserRegisterView.as_view(), name="register"),
 ]
