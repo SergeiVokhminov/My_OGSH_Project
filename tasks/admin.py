1f98d1efcba2f72ded51a080d64aa753ddc150f0
sergei_vokhminov <rreaper@list.ru>
2025-04-27 17:10:30 +0300

    добавлено отображение приложения tasks в админ-части

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from tasks.models import Task
+from users.models import User
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    """Настройки отображения модели Task в админ-панели Django."""
+
+    list_display = ("id", "title", "parent_task", "status", "deadline")
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        # Исключаем администратора из списка исполнителей
+        if db_field.name == "employee":
+            kwargs["queryset"] = User.objects.exclude(is_superuser=True)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
