1870e94c2a6689c84f9da62c041aed0ca89f176d
sergei_vokhminov <rreaper@list.ru>
2025-04-27 16:43:43 +0300

    обновлен модуль urls приложения users

--- users/urls.py
+++ users/urls.py
@@ -3,16 +3,21 @@ from django.urls import path
 
 from users.apps import UsersConfig
 from users.views import (
-    HomeView, UserRegisterView, UserDetailsView, UserUpdateView
+    HomeView, UserRegisterView, UserDetailsView, UserInfoView, UserListView, UserDeleteView, UserUpdateView
 )
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
+
     path("login/", LoginView.as_view(template_name="users/login.html"), name="login"),
     path("logout/", LogoutView.as_view(next_page="users:home"), name="logout"),
     path("register/", UserRegisterView.as_view(), name="register"),
-    path("detail/<int:pk>/", UserDetailsView.as_view(), name="user_detail"),
-    path("update/<int:pk>/", UserUpdateView.as_view(), name="test"),
+
+    path("user/info/<int:pk>/", UserInfoView.as_view(), name="user_info"),
+    path("user/list/", UserListView.as_view(), name="user_list"),
+    path("user/detail/<int:pk>/", UserDetailsView.as_view(), name="user_detail"),
+    path("user/update/<int:pk>/", UserUpdateView.as_view(), name="user_update"),
+    path("user/delete/<int:pk>/", UserDeleteView.as_view(), name="user_delete"),
 ]
