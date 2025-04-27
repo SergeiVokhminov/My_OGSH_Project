590f5e7b0e6ad5f9bd8154dbaa2023b96f79e98f
sergei_vokhminov <rreaper@list.ru>
2025-04-27 18:07:44 +0300

    обновлен модуль urls всего проекта

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,7 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("users.urls"), name="users"),
+    path("tasks", include("tasks.urls"), name="tasks"),
 ]
 
 if settings.DEBUG:
