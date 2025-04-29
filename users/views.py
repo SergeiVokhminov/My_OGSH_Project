7b8263d66b3e6771e60bbe5a322274c80beff30a
sergei_vokhminov <rreaper@list.ru>
2025-04-29 11:13:24 +0300

    обновлен модуль приложения users

--- users/views.py
+++ users/views.py
@@ -17,7 +17,7 @@ class UserInfoView(UpdateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["task_count"] = Task.objects.count() # Считаем количество пользователей
+        context["task_count"] = Task.objects.count()  # Считаем количество пользователей
 
         return context
 
