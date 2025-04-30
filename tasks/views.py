efde5530f4833720f90c39bd0ce89ad64fc5396e
sergei_vokhminov <rreaper@list.ru>
2025-04-30 16:13:40 +0300

    обновлены представления приложения tasks

--- tasks/views.py
+++ tasks/views.py
@@ -3,9 +3,12 @@
 # from django.conf import settings
 # from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 # from django.http import HttpResponseForbidden
+from django.db.models import Count, Q
+from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from django.views.generic import DetailView, ListView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from requests import Response
 
 from tasks.forms import TaskUpdateForm
 from tasks.models import Task
@@ -42,7 +45,12 @@ class TaskListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["task_count"] = Task.objects.count() # Считаем количество пользователей
+        # Посчитаем количество задач по каждому статусу
+        context["task_count"] = Task.objects.count()  # Считаем количество задач
+        context["start_task_count"] = Task.objects.filter(status="start").count()
+        context["free_task_count"] = Task.objects.filter(status="free").count()
+        context["done_task_count"] = Task.objects.filter(status="done").count()
+        context["closed_task_count"] = Task.objects.filter(status="closed").count()
 
         return context
 
