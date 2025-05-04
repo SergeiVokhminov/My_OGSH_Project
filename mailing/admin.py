761336d5bb463a76b24ddd2ec776b3563b576360
sergei_vokhminov <rreaper@list.ru>
2025-05-04 19:09:48 +0300

    добавлены маршруты приложения mailing

--- mailing/admin.py
+++ mailing/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from mailing.models import Recipient, Message
+
+
+@admin.register(Recipient)
+class RecipientAdmin(admin.ModelAdmin):
+    list_display = ("id", "email", "first_name", "last_name", "patronymic", "comment")
+    search_fields = ("email", "last_name")
+    list_filter = ("email", "last_name")
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("id", "topic", "text")
+    search_fields = ("topic",)
+    list_filter = ("topic",)
---
+++ mailing/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path
+from django.views.decorators.cache import cache_page
+
+from mailing.apps import MailingConfig
+from mailing.views import (HomeView, MainPageView, MessageListView, MessageDetailsView, MessageCreateView,
+                           MessageUpdateView, MessageDeleteView, MailingListView, MailingCreateView, MailingDetailsView,
+                           MailingUpdateView, MailingDeleteView, RecipientListView, RecipientDetailsView,
+                           RecipientCreateView, RecipientUpdateView, RecipientDeleteView, MessageInfoView,
+                           MailingInfoView, RecipientInfoView, MailingAttemptsListView, finish_mailing, sending_mail,
+                           ReportInfoView)
+
+app_name = MailingConfig.name
+
+urlpatterns = [
+    path("message_info/", MessageInfoView.as_view(), name="message_info"),
+    path('message_list/', cache_page(60)(MessageListView.as_view()), name='message_list'),
+    path("message/create/", MessageCreateView.as_view(), name="message_create"),
+    path('message/<int:pk>/', MessageDetailsView.as_view(), name='message_detail'),
+    path("message/<int:pk>/update/", MessageUpdateView.as_view(), name="message_update"),
+    path("message/<int:pk>/delete/", MessageDeleteView.as_view(), name="message_delete"),
+
+    path("recipient_info/", RecipientInfoView.as_view(), name="recipient_info"),
+    path('recipient_list/', cache_page(60)(RecipientListView.as_view()), name='recipient_list'),
+    path("recipient/create/", RecipientCreateView.as_view(), name="recipient_create"),
+    path('recipient/<int:pk>/', RecipientDetailsView.as_view(), name='recipient_detail'),
+    path("recipient/<int:pk>/update/", RecipientUpdateView.as_view(), name="recipient_update"),
+    path("recipient/<int:pk>/delete/", RecipientDeleteView.as_view(), name="recipient_delete"),
+
+]
