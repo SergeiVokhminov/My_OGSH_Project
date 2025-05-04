98da7535bbaf657d21bd3153a9ac1c83dd1e43ca
sergei_vokhminov <rreaper@list.ru>
2025-05-04 17:14:07 +0300

    обновлены маршруты приложения applications

---
+++ applications/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+
+from applications.apps import ApplicationsConfig
+from applications.views import (
+    ApplicationCreateView,
+    ApplicationDeleteView,
+    ApplicationListView,
+    ApplicationUpdateView,
+    ApplicationDetailsView,
+    ApplicationHomeView,
+)
+
+app_name = ApplicationsConfig.name
+
+urlpatterns = [
+    path("info", ApplicationHomeView.as_view(), name="application_info"),
+    path("create/", ApplicationCreateView.as_view(), name="application_create"),
+    path("list/", ApplicationListView.as_view(), name="application_list"),
+    path("detail/<int:pk>/", ApplicationDetailsView.as_view(), name="application_detail"),
+    path("update/<int:pk>/", ApplicationUpdateView.as_view(), name="application_update"),
+    path("delete/<int:pk>/", ApplicationDeleteView.as_view(), name="application_delete"),
+]
