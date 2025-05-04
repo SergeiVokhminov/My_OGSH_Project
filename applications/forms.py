cd2d759b82ec6bc0157b897979760d238f1f732a
sergei_vokhminov <rreaper@list.ru>
2025-05-04 17:13:02 +0300

    добавлена форма модели application

---
+++ applications/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+
+from applications.models import Application
+
+
+class ApplicationUpdateForm(forms.ModelForm):
+    """Форма для задачи."""
+
+    class Meta:
+        model = Application
+        fields = (
+            "title",
+            "description",
+            "deadline",
+        )
+
+    def __init__(self, *args, **kwargs):
+        super(ApplicationUpdateForm, self).__init__(*args, **kwargs)
+        self.fields["title"].widget.attrs.update(
+            {"class": "form-control", "placeholder": "Введите название заявки"}
+        )
+        self.fields["description"].widget.attrs.update(
+            {"class": "form-control", "placeholder": "Введите описание заявки"}
+        )
+        self.fields["deadline"].widget.attrs.update(
+            {"class": "form-control", "placeholder": "Введите срок исполнения заявки"}
+        )
