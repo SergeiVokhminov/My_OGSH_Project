29cb634ff382786d16df63c701c1d3754c8e835a
sergei_vokhminov <rreaper@list.ru>
2025-05-04 15:35:24 +0300

    выполнены миграции модели приложения applications

--- applications/models.py
+++ applications/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Application(models.Model):
+    """Поля для модели заявки."""
+
+    title = models.CharField(
+        max_length=250,
+        verbose_name="Название заявки",
+    )
+    description = models.TextField(
+        verbose_name="Описание заявки",
+        null=True,
+        blank=True,
+    )
+    deadline = models.DateField(
+        verbose_name="Дата исполнения",
+        null=True,
+        blank=True,
+    )
+    created_at = models.DateField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateField(auto_now=True, verbose_name="Дата обновления")
+
+    def __str__(self):
+        return f"{self.title}"
+
+    class Meta:
+        verbose_name = "Заявка"
+        verbose_name_plural = "Заявки"
