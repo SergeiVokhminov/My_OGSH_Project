93a8512b733b6f93ecd9778efa5ea6312b574563
sergei_vokhminov <rreaper@list.ru>
2025-05-05 14:55:44 +0300

    обновлен модуль представления приложения tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,14 +1,7 @@
-# from datetime import datetime
-#
-# from django.conf import settings
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-# from django.http import HttpResponseForbidden
-from django.db.models import Count, Q
-from django.shortcuts import render
-from django.urls import reverse, reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 from django.views.generic import DetailView, ListView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
-from requests import Response
 
 from tasks.forms import TaskUpdateForm
 from tasks.models import Task
