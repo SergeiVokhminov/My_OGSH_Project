b844ff8b4121ca8e88cc4ac66c9244342989d338
sergei_vokhminov <rreaper@list.ru>
2025-05-10 22:52:45 +0300

    корректировка форм приложения applications

--- applications/forms.py
+++ applications/forms.py
@@ -3,8 +3,19 @@ from django import forms
 from applications.models import Application
 
 
-class ApplicationUpdateForm(forms.ModelForm):
+class ApplicationForm(forms.ModelForm):
     """Форма для задачи."""
+
+    class Meta:
+        model = Application
+        exclude = ("title", "description", "deadline", "times")
+
+    def __init__(self, *args, **kwargs):
+        super(ApplicationForm, self).__init__(*args, **kwargs)
+
+
+class ApplicationUpdateForm(forms.ModelForm):
+    """Форма для обновления заявки."""
 
     class Meta:
         model = Application
@@ -23,7 +34,7 @@ class ApplicationUpdateForm(forms.ModelForm):
             {"class": "form-control", "placeholder": "Введите описание заявки"}
         )
         self.fields["deadline"].widget.attrs.update(
-            {"class": "form-control", "placeholder": "Введите срок исполнения заявки"}
+            {"class": "form-control", "placeholder": "Введите срок исполнения заявки в формате 00.00.0000"}
         )
 
 
