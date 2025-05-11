70d4636f1431d12fb3905dfbd0400b4dc520ff79
sergei_vokhminov <rreaper@list.ru>
2025-05-11 09:10:27 +0300

    обновлены представления приложения tasks

--- tasks/views.py
+++ tasks/views.py
@@ -5,6 +5,7 @@ from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from tasks.forms import TaskUpdateForm
 from tasks.models import Task
+from tasks.utils import TaskCounter
 
 
 class TaskCreateView(CreateView):
@@ -38,15 +39,12 @@ class TaskListView(LoginRequiredMixin, ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["task_count"] = Task.objects.count()  # Считаем общее количество созданных задач
-        context["start_task_count"] = Task.objects.filter(
-            status="start"
-        ).count()  # Считаем количество задач "К исполнению"
-        context["free_task_count"] = Task.objects.filter(status="free").count()  # Считаем количество задач "Свободна"
-        context["done_task_count"] = Task.objects.filter(status="done").count()  # Считаем количество задач "Завершена"
-        context["closed_task_count"] = Task.objects.filter(
-            status="closed"
-        ).count()  # Считаем количество задач "Отменена"
+        counter = TaskCounter()
+        context["task_count"] = counter.total()  # Считаем общее количество созданных задач
+        context["start_task_count"] = counter.start_task  # Считаем количество задач "К исполнению"
+        context["free_task_count"] = counter.free_task  # Считаем количество задач "Свободна"
+        context["done_task_count"] = counter.done_task  # Считаем количество задач "Завершена"
+        context["closed_task_count"] = counter.closed_task  # Считаем количество задач "Отменена"
 
         return context
 
