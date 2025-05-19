0952968126f22b690f89d92377ab17eee995739a
sergei_vokhminov <rreaper@list.ru>
2025-05-19 14:52:51 +0300

    добавлена модель в приложении departments

--- departments/models.py
+++ departments/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Department(models.Model):
+    """Поля для модели отдела."""
+
+    DEPARTMENT_CHOICES = [
+        ("OGESh", "ОЭГШ"),
+        ("SB", "CБ"),
+        ("SIT", "СИТ"),
+        ("OGM", "ОГМ"),
+        # Добавить другие отделы при необходимости
+    ]
+    name = models.CharField(
+        max_length=50,
+        choices=DEPARTMENT_CHOICES,
+        verbose_name="Название Отдела"
+    )
+
+    def __str__(self):
+        """Метод для строкового представления объекта Department."""
+
+        return f"{self.name}"
+
+    class Meta:
+        """Мета-информация модели Department."""
+
+        verbose_name = "Отдел"
+        verbose_name_plural = "Отделы"
