136bf346458e5d92a572cad6b5ba67a70f4c3e1d
sergei_vokhminov <rreaper@list.ru>
2026-04-16 20:48:01 +0300

    корректировка модуля utils приложения users

--- users/utils.py
+++ users/utils.py
@@ -8,33 +8,6 @@ class UserCounter:
         self.queryset = queryset
 
     def total(self):
-        """Определяем общее количество пользователей."""
+        """Определяем общее количество зарегистрированных пользователей."""
 
         return self.queryset.count()
-
-    def count_by_condition(self, condition):
-        """Определяем количество пользователей по статусу сотрудника."""
-
-        return self.queryset.filter(condition=condition).count()
-
-    @property
-    def at_work(self):
-        """Определяем количество пользователей со статусом 'На работе'."""
-
-        return self.count_by_condition("work")
-
-    @property
-    def on_vacation(self):
-        """Определяем количество пользователей со статусом 'В отпуске'."""
-
-        return self.count_by_condition("vacation")
-
-    @property
-    def on_sick_leave(self):
-        """Определяем количество пользователей со статусом 'На больничном'."""
-        return self.count_by_condition("sick_leave")
-
-    @property
-    def truancy(self):
-        """Определяем количество пользователей со статусом 'Прогул'."""
-        return self.count_by_condition("truancy")
