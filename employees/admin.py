e04aca1cd1da9a63db23be35fdc89d15d4f10ee1
sergei_vokhminov <rreaper@list.ru>
2026-04-17 11:27:02 +0300

    корректировка модели и модуля admin приложения employees

--- employees/admin.py
+++ employees/admin.py
@@ -13,6 +13,7 @@ class UserAdmin(admin.ModelAdmin):
         "last_name",
         "first_name",
         "position",
+        "is_active",
     )
     list_filter = ("id", "last_name")
     search_fields = ("email", "last_name", "phone_number")
